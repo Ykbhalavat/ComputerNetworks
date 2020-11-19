43d78e17dff92dda37f690915cd4f1f6862cf476
Ykbhalavat <44511550+Ykbhalavat@users.noreply.github.com>
2020-11-19 12:52:04 -0600

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,58 +1,54 @@
-
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.DataInputStream; 
+import java.io.DataOutputStream; 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-
-//import jdk.internal.module.Resources;
+import java.io.PrintWriter;
+import java.net.InetAddress; 
+import java.net.Socket; 
+import java.net.UnknownHostException; 
+import java.util.Scanner; 
+  
 
 public class Client {
   private Socket socket = null;
   private BufferedReader dataInput = null;
-  private PrintStream dataOutput = null;
+  private DataInputStream Serverreaddata = null;
+  private DataOutputStream dataOutput = null; 
 
-  public Client(String address, int portNum) {
-    // start the connection process
-    try {
+
+  public Client(String address, int portNum) throws IOException
+  {
+    //start the connection process
+   
       socket = new Socket(address, portNum);
       dataInput = new BufferedReader(new InputStreamReader(System.in));
-      dataOutput = new PrintStream(socket.getOutputStream());
+      Serverreaddata =   new DataInputStream((socket.getInputStream()));
+      dataOutput = new DataOutputStream(socket.getOutputStream());
       System.out.println("Connection to Server Successful");
-    } catch (UnknownHostException hostExecpt) {
-      System.out.println(hostExecpt);
-    } catch (IOException IO) {
-      System.out.println(IO);
+      
+ while(true)
+ {
+	    System.out.println("Enter expression to be calculated, or type 'end' to end connection to server: ");
+	    String response = dataInput.readLine();
+
+        if (response.equals("bye")) 
+            break; 
+
+        dataOutput.writeUTF(response); 
+
+        String ans = Serverreaddata.readUTF(); 
+        
+        System.out.println("Answer=" + ans); 
+ }
+    
+  
+  }  
+  
+  public static void main(String[] args) throws IOException
+    {
+      Client testClient = new Client("127.0.0.1", 5000);
+      
     }
-
-    String userInput = "";
-    System.out.println("Enter expression to be calculated, or type 'end' to end connection to server: ");
-
-    // The while loop that will keep asking for user input
-    while (!userInput.equals("end")) {
-      try {
-        userInput = dataInput.readLine();
-        dataOutput.println(userInput);
-      } catch (IOException IO) {
-        System.out.println(IO);
-      }
-    }
-
-    // close all open streams
-    try {
-      dataInput.close();
-      dataOutput.close();
-      socket.close();
-    } catch (IOException IO) {
-      System.out.println(IO);
-    }
-  }
-
-  public static void main(String[] args) {
-    Client testClient = new Client("127.0.0.1", 5000);
-  }
 }
+
